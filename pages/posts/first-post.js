3ce4289378afa3fd379bda97fdcc14f308661ec5
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2021-05-23 19:25:46 +0200

    Next.js image and head components added.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,19 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First post</title>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">
-                    <a>Back to home</a>
+                    <a>Back to home page</a>
                 </Link>
             </h2>
+            <Image src={"/images/profile.jpg"} height={240} width={360} alt={"My image"}/>
         </>
     )
 }
