352c2948e1dce22450c41dcbda6e839deeef022f
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2021-05-26 00:01:37 +0200

    Dynamic routes added (posts ids)

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,6 @@ export default function Home({allPostsData}) {
                 <Link href="/posts/first-post">
                     <a className={utilStyles.colorInherit}>First post</a>
                 </Link>
-                <p>
-                    (This is a sample website - you’ll be building a site like this on{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
